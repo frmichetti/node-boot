6f283af9cc18655507d1a9d1632966669b5d73a6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-18 13:20:13 -0300

    added express

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import 'dotenv/config';
-import axios from 'axios';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
 
-(async () => {
+process.env.TZ = 'America/Sao_Paulo';
 
-  try {
-    const response = await axios.get(`https://api.github.com/users/${process.env.GITHUB_USER}`)
-    console.log(response.data)
-  } catch (error) {
-    console.error(error)
-  }
+const app = express();
 
-})()
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
+process.on('uncaughtException', function (error) {
+  console.log(error.stack);
+  console.log("uncaughtException Node NOT Exiting...");
+});
+
+const port = process.env.PORT || 4000;
+
+app.get('/welcome', (req, res, next) => {
+  res.status(200).send({ message: 'Welcome' })
+});
+
+app.post('/welcome', (req, res, next) => {
+  res.status(200).send(req.body)
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});

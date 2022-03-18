7b1852d48fcea319ce65420d5505879a46ae5ef6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-18 13:00:53 -0300

    added test with twilio

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import axios from 'axios';
 
 (async () => {
 
-  try {
-    const response = await axios.get(`https://api.github.com/users/${process.env.GITHUB_USER}`)
-    console.log(response.data)
-  } catch (error) {
-    console.error(error)
-  }
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
 
-})()
+  client.messages
+    .create({
+      from: 'whatsapp:+14155238886',
+      body: 'Hello from NODE',
+      to: 'whatsapp:+19999785738'
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+})();
 

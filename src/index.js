91381da0de172823fd0003ec9cc4fbfe17104886
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-18 15:54:00 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -7,13 +7,23 @@ import axios from 'axios';
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require('twilio')(accountSid, authToken);
 
-  client.messages
+  // Send a Message
+  /*client.messages    
     .create({
+      outcome: 'confirmed',
       from: 'whatsapp:+14155238886',
       body: 'Hello from NODE',
-      to: 'whatsapp:+19999785738'
+      to: 'whatsapp:+5519numero'
     })
     .then(data => console.log(data))
+    .catch(error => console.error(error));*/
+
+
+  // GET MESSAGE Feedback URL  
+  client.messages('messageSid')
+    .feedback
+    .create({ outcome: 'confirmed' })
+    .then(feedback => console.log(feedback))
     .catch(error => console.error(error));
 })();
 
